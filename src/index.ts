d64e10f6b49f50acfb14da7a4d2516c5694e7d5a
lakhani milan <lakhanimilan99@gmail.com>
2023-07-12 10:50:08 +0530

    add crash log

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,20 @@ app.use(morgan('dev'));
 
 app.use('/api/student', studentRoutes);
 
+process.on('uncaughtException', (error, origin) => {
+  console.log('----- Uncaught exception -----')
+  console.log(error)
+  console.log('----- Exception origin -----')
+  console.log(origin)
+})
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.log('----- Unhandled Rejection at -----')
+  console.log(promise)
+  console.log('----- Reason -----')
+  console.log(reason)
+})
+
 sequelizeInstance
   .authenticate()
   .then(async () => {
